341233adda66663decc9c1adf4df75793e3e38e7
Chupa14142 <chupa14142@gmail.com>
2022-01-12 12:04:07 +0200

    ex14

--- lib/base_case.py
+++ lib/base_case.py
@@ -18,5 +18,4 @@ class BaseCase:
             assert False, f"Response is not in Json Format. Response text is '{response.text}'"
 
         assert name in response_as_dict, f"Response JSON doesn't have key '{name}'"
-
         return response_as_dict[name]
